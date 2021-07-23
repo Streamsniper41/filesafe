435bc9548c2e908bdf1e7baf0fcb8d447118609a
Tom McCormack <mccormaclt411@toms-macbook-air.home>
2021-07-23 22:16:50 +0200

    finished getfiles function

--- app.py
+++ app.py
@@ -16,17 +16,19 @@ def test():
 # getfiles function: gets list of files in database and returns them to the client 
 @app.route("/getfiles")
 def getfiles():
-   print(os.listdir(path='DB/'))
-   return "yes"
+   return f"{os.listdir(path='DB/')}"
 
 
 # download file from database 
-@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    # Appending app path to upload folder path within app root folder
-    uploads = os.path.join(current_app.root_path, app.config['DB/'])
-    # Returning file from appended path
-    return send_from_directory(directory=uploads, filename=filename)
+@app.route('/uploads/<path>', methods=['GET', 'POST'])
+def download(path):
+    return f"hello {path}!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
+
+
