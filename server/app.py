40349e2bd861271c9b705703b235bc7fbe974056
Tom McCormack <mccormaclt411@toms-macbook-air.home>
2021-07-27 00:00:29 +0200

    file upload feature

--- server/app.py
+++ server/app.py
@@ -1,13 +1,23 @@
 from flask import Flask
-import os, sys
+import os
+import sys
 from flask import render_template_string
 from flask import redirect
 from flask import request
 import subprocess
 import subprocess
 import shutil
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.getcwd()
+
 # HTML PAGE
+
+
 @app.route('/')
 def root():
     return render_template_string('''
@@ -23,6 +33,11 @@ def root():
             
             
             <ul>
+             <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+
              <form action="/touch">
                 <input type="submit" value="New file"/>
                 <input name="filename" type="text" value="new_file.txt"/>
@@ -46,32 +61,38 @@ def root():
           </body>
         </html>
     ''', current_working_directory=os.getcwd(),
-         file_list=subprocess.check_output('ls', shell=True).decode('utf-8').split('\n')) # use 'dir' command on Windows
-    
+                                  file_list=subprocess.check_output('ls', shell=True).decode('utf-8').split('\n'))  # use 'dir' command on Windows
+
 # handle 'cd' command
+
+
 @app.route('/cd')
 def cd():
     # run 'level up' command
     os.chdir(request.args.get('path'))
-    
+
     # redirect to file manager
     return redirect('/')
 
 # handle 'make directory' command
+
+
 @app.route('/md')
 def md():
     # create new folder
     os.mkdir(request.args.get('folder'))
-    
+
     # redirect to file manager
     return redirect('/')
 
     # handale 'make file' command
+
+
 @app.route('/touch')
 def touch():
-    # create file 
+    # create file
     open(request.args.get('filename'), 'a+').close()
-    #redirect to file manager
+    # redirect to file manager
     return redirect('/')
 
 
@@ -80,11 +101,13 @@ def touch():
 def rm():
     # remove certain directory
     shutil.rmtree(os.getcwd() + '/' + request.args.get('dir'))
-    
+
     # redirect to fole manager
     return redirect('/')
-    
+
 # view text files
+
+
 @app.route('/view')
 def view():
     # get the file content
@@ -92,31 +115,53 @@ def view():
         return f.read().replace('\n', '<br>')
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 # @app.route("/1")
 # def test():
 #     f = open('test.txt', 'r')
 #     f_content = f.read()
 #     return f_content
-#     f.close()    
+#     f.close()
 
 
-# # getfiles function: gets list of files in database and returns them to the client 
+# # getfiles function: gets list of files in database and returns them to the client
 # @app.route("/getfiles")
 # def getfiles():
 #    return f"{os.listdir(path='DB/')}"
 
 
-# download file from database 
-@app.route('/uploads/<path>', methods=['GET', 'POST'])
-def download(path):
-    return f"hello {path}!"
+# download file from database
+# @app.route('/uploads/<path>', methods=['GET', 'POST'])
+# def download(path):
+#     return f"hello {path}!"
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-
-
-
-
-
